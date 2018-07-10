6abebc2f71946d2afd5d9e2a5c22496b811af884
sivasai2467 <40885720+sivasai2467@users.noreply.github.com>
2018-07-10 11:14:52 +0530

    Update armstg.py

--- armstg.py
+++ armstg.py
@@ -1,7 +1,7 @@
 n=int(input())
-a=list(map(int,str(n)))
-b=list(map(lambda x:x**3,a))
-if(sum(b)==n):
+o=list(map(int,str(n)))
+p=list(map(lambda x:x**3,o))
+if(sum(p)==n):
     print("yes")
 else:
     print("no")
