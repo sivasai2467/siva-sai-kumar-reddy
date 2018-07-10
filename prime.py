7bdd957919dc3a1233cc6f61790cbabb48c41224
sivasai2467 <40885720+sivasai2467@users.noreply.github.com>
2018-07-10 09:49:10 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,9 @@
-n = int(input())
-if (n % 2) == 1:
-   print("yes")
+n=int(input())
+p=0
+for i in range(2,n//2+1):
+    if(n%i==0):
+        p=p+1
+if(p<=0):
+    print("yes")
 else:
-   print("no")
+    print("no")
