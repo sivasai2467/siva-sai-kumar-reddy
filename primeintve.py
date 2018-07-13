d7e79ec220f527a177a6c71724bd07f178ce4450
sivasai2467 <40885720+sivasai2467@users.noreply.github.com>
2018-07-13 10:53:01 +0530

    Create primeintve.py

---
+++ primeintve.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().strip().split())
+for num in range(a,b + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num,end=" ")
