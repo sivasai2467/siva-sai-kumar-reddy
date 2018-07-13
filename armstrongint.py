96d3de6efad9233479ed5754141da61bcdf8c43d
sivasai2467 <40885720+sivasai2467@users.noreply.github.com>
2018-07-13 11:05:02 +0530

    Create armstrongint.py

---
+++ armstrongint.py
@@ -0,0 +1,12 @@
+x,y=map(int,input().strip().split())
+ 
+for num in range(x,y + 1):
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num == sum:
+       print(num,end="")
